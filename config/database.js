35c154327d1164b4a74492ed4517ba016e353982
Mohamed Jaris <jarismohamed41@gmail.com>
2026-07-29 00:45:36 +0530

    second push

--- config/database.js
+++ config/database.js
@@ -1,10 +1,28 @@
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const path = require("path");
 
-const sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: path.join(__dirname, "../database/textile.sqlite"),
-    logging: false
-});
+const usingDatabaseUrl = Boolean(process.env.DATABASE_URL);
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || {
+    database: process.env.DB_NAME || "textile_stock",
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
+    dialect: "postgres",
+  },
+  {
+    logging: false,
+    dialectOptions: usingDatabaseUrl
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
+  }
+);
 
 module.exports = sequelize;
